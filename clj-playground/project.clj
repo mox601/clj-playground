95901ec69bed6bed52ae7482522ae19502d034dc
Matteo Moci <mox601@gmail.com>
2018-01-14 22:41:59 +0100

    initial commit

---
+++ clj-playground/project.clj
@@ -0,0 +1,8 @@
+(defproject clj-playground "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/mox601/clj-playground"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/test.check "0.10.0-alpha2"]
+                 [csv-map "0.1.2"]])
