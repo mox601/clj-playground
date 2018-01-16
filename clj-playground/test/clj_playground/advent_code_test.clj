0a80941d5d22ffe9f8d64d6537830763c7193768
Matteo Moci <mox601@gmail.com>
2018-01-16 22:43:39 +0100

    added failing tests

--- clj-playground/test/clj_playground/advent_code_test.clj
+++ clj-playground/test/clj_playground/advent_code_test.clj
@@ -3,11 +3,15 @@
 
 (defn captcha
   [x]
-  (str x "a"))
+  (str x)
+  3)
 
 (deftest day-1-test
   (testing "first digit (1) matches the second digit and the third digit (2) matches the fourth digit"
     (is (= (captcha "1122") 3)))
-  (testing "each digit (all 1) matches the next")
-  (testing "because no digit matches the next")
-  (testing "the only digit that matches the next one is the last digit, 9."))
+  (testing "each digit (all 1) matches the next"
+    (is (= (captcha "1111") 4)))
+  (testing "because no digit matches the next"
+    (is (= (captcha "1234") 0)))
+  (testing "the only digit that matches the next one is the last digit, 9."
+    (is (= (captcha "91212129") 9))))
