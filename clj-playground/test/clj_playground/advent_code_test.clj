05cc84f60759b912717372e5de581aed483ecaf1
Matteo Moci <mox601@gmail.com>
2018-01-16 21:42:16 +0100

    added test cases

--- clj-playground/test/clj_playground/advent_code_test.clj
+++ clj-playground/test/clj_playground/advent_code_test.clj
@@ -6,5 +6,8 @@
   (str x "a"))
 
 (deftest day-1-test
-  (testing "fixed, I succeed."
-    (is (= (captcha "") "a"))))
+  (testing "first digit (1) matches the second digit and the third digit (2) matches the fourth digit"
+    (is (= (captcha "1122") 3)))
+  (testing "each digit (all 1) matches the next")
+  (testing "because no digit matches the next")
+  (testing "the only digit that matches the next one is the last digit, 9."))
