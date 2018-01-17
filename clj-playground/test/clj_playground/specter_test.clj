a84617790d44da6ae70c661297f01e862ace7720
Matteo Moci <mmoci@expedia.com>
2018-01-17 11:31:42 +0100

    minor

--- clj-playground/test/clj_playground/specter_test.clj
+++ clj-playground/test/clj_playground/specter_test.clj
@@ -2,15 +2,15 @@
   (:require [clojure.test :refer :all]
             [com.rpl.specter :refer :all]))
 
+(def data {:a [{:aa 1 :bb 2}
+               {:cc 3}]
+           :b [{:dd 4}]})
+
+(def inc-data {:a [{:aa 1 :bb 3}
+                   {:cc 3}]
+               :b [{:dd 5}]})
+
 (deftest a-specter-test
-  (def data {:a [{:aa 1 :bb 2}
-                 {:cc 3}]
-             :b [{:dd 4}]})
-
-  (def inc-data {:a [{:aa 1 :bb 3}
-                     {:cc 3}]
-                 :b [{:dd 5}]})
-
   (testing "specter"
     (is (= (transform [MAP-VALS ALL MAP-VALS even?] inc data)
            inc-data))))
