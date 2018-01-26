2ee07ed2374b3433d276d2903f7a6e840c4cb4fb
Matteo Moci <mox601@gmail.com>
2018-01-26 08:08:36 +0100

    fixed fn

--- clj-playground/test/clj_playground/advent_code_test.clj
+++ clj-playground/test/clj_playground/advent_code_test.clj
@@ -36,9 +36,7 @@
 
 (defn split-lines-and-items
   [x]
-  (map #(s/split % #"\t") (s/split-lines x)))
-
-;;(split-lines-and-items "a\tb\tc\nd\te\tf")
+  (map (map #(s/split % #"\s+") (s/split-lines x))))
 
 (defn max-and-min
   [x]
@@ -47,6 +45,10 @@
 (defn difference-between-max-and-min
   [x]
   (apply - (max-and-min x)))
+
+;;(defn difference-between-max-and-min-tbl
+;;  [x]
+;;  (map #(apply - %) (map max-and-min x)))
 
 (defn checksum
   [x]
@@ -64,7 +66,4 @@
       (is (= (checksum "5 1 9 5
 7 5 3
 2 4 6 8") 18))))
-
-
-
 
