b9a7cbd1af6c5bd2b644c36a7b5860f112502f3f
Matteo Moci <mox601@users.noreply.github.com>
2019-12-27 08:42:29 +0100

    lop

--- clj-playground/test/clj_playground/advent_code_2019_test.clj
+++ clj-playground/test/clj_playground/advent_code_2019_test.clj
@@ -20,7 +20,7 @@
 
 (defn loop-until-negative
   [f m]
-  (loop [x m
+  (loop [x  m
          xs []]
     (if (< x 0)
       xs
@@ -34,10 +34,11 @@
   (testing "day-1"
     (is (= (sum-of-map-fn fuel-requirement day-1-input-seq)
            3369286))
-    (is (= (sum-of-map-fn #(reduce + 0 (drop-last (loop-until-negative fuel-requirement %)))  day-1-input-seq)
+    (is (= (sum-of-map-fn #(reduce + 0 (drop-last (loop-until-negative fuel-requirement %))) day-1-input-seq)
            5051054))))
 
-(def day-2-input (slurp (io/resource "resources/advent2019/day-2.txt")))
+(def day-2-input
+  (slurp (io/resource "resources/advent2019/day-2.txt")))
 
 (def day-2-input-seq (s/split day-2-input #"[,\n]"))
 
@@ -50,13 +51,6 @@
   [mem noun verb]
   (assoc mem 1 noun 2 verb))
 
-;; (program mem n v)
-
-;; read at index
-;; (nth [0 1 2] 2)
-
-;; substitute at index
-;; (assoc [0 1 2] 1 2)
 ;; opcode 1 2 99
 (defn opcode [n]
   (if (= 1 n)
@@ -64,12 +58,10 @@
     (when (= 2 n)
       *)))
 
-;; next index + 4
 (defn next-idx
   [n]
   (+ 4 n))
 
-;; takes also operation offset
 (defn process-opcode
   [v offset]
   (let [[w x y z] (drop offset v)]
@@ -85,9 +77,6 @@
       v
       (recur (process-opcode v offset) (next-idx offset)))))
 
-;; noun index 1
-;; verb index 2
-
 ;; determine what pair of inputs produces the output 19690720
 
 (defn calculate
@@ -98,22 +87,32 @@
   [noun verb]
   (+ (* 100 noun) verb))
 
-
 (defn cartesian-product
   [s]
   (for [x s
         y s]
     (vector x y)))
+
 ;; works
 ;; calculate program with input cartesian product
 ;; until output is 19690720
 
-(defn iterate-until
-  [])
+(defn calculate-until
+  [vs start end]
+  (loop [pairs (cartesian-product (range start end))
+         offset 0]
+    (when (> (count pairs) offset)
+      (let [[noun verb] (nth pairs offset)]
+        (if (= 19690720 (calculate (program vs noun verb)))
+          (mult-plus noun verb)
+          (recur pairs (inc offset)))))))
+
+;;works? 5936 (calculate-until day-2-input-vec-ints 1 100)
 
 (deftest day-2-test
   (testing "day-2"
     (is (= (count day-2-input-seq) 165))
-    (is (= (calculate (program day-2-input-vec-ints 12 2))) 4462686)))
+    (is (= (calculate (program day-2-input-vec-ints 12 2))) 4462686)
+    (is (= (calculate-until day-2-input-vec-ints 1 100) 5936))))
 
 
